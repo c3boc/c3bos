52fd928a310232e055a61b2ebf34e3e60c628e19
Felix Favre <felix.favre@protonet.info>
2015-08-26 17:14:20 +0200

    added user registration form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get 'sign_up' => "users#new", :as => "sign_up"
+  root :to => "users#new"
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
