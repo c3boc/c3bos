e3cc2ecb246e54eb5b58fff4062382223d40c98a
Felix Favre <felix.favre@protonet.info>
2015-09-14 18:44:19 +0200

    create, delete and index locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,6 +2,29 @@ class LocationsController < ApplicationController
 
   def index
     @locations = Location.all
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(user_params)
+
+    if @location.save
+      redirect_to locations_path, :info => "Location added"
+    else
+      redirect_to locations_path, :danger => "Location not added"
+    end
+  end
+
+  def destroy
+    @location = Location.find(params[:id])
+    @location.destroy
+
+    redirect_to locations_path, :info => "Location removed"
+  end
+
+  private
+  def user_params
+    params.require(:location).permit(:title, :room)
   end
 
 end
