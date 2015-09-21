a541e16e2a2722c3281e6ab7cfafe823ec75ae35
Felix Favre <felix.favre@protonet.info>
2015-09-21 19:01:12 +0200

    set appname repo url and rbenv version in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,9 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'c3bos'
+set :repo_url, 'git@github.com:c3boc/c3bos.git'
+set :rbenv_ruby, '2.2.3'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
