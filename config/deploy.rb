1afc9f12e7f96de28184cb4a40a08d8862dfbf8e
Felix Favre <felix.favre@protonet.info>
2015-09-21 21:49:25 +0200

    change linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'c3bos'
 set :repo_url, 'git@github.com:c3boc/c3bos.git'
 set :rbenv_ruby, '2.2.3'
 
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, ['config/database.yml', 'config/secrets.yml']
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
