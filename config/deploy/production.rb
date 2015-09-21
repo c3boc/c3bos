e2e19432811495857c5048d38515975180121fbe
Felix Favre <felix.favre@protonet.info>
2015-09-21 19:01:33 +0200

    add production server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
+server 'gnomus.de', user: 'gnomus', roles: %w{web app}
 
 
 # role-based syntax
