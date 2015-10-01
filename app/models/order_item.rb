794a7a9c86df956750b2c68598fa608fef3c1578
Felix Favre <felix.favre@protonet.info>
2015-10-01 15:42:26 +0200

    handle order_item.amount = nil

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -4,13 +4,13 @@ class OrderItem < ActiveRecord::Base
 
   after_save do |order_item|
     beverage = Beverage.find(order_item.beverage)
-    beverage.in_storage = beverage.in_storage - order_item.amount
+    beverage.in_storage = beverage.in_storage - order_item.amount if order_item.amount
     beverage.save
   end
 
   before_destroy do |order_item|
     beverage = Beverage.find(order_item.beverage)
-    beverage.in_storage = beverage.in_storage + order_item.amount
+    beverage.in_storage = beverage.in_storage + order_item.amount if order_item.amount
     beverage.save
   end
 end
