16e16a6334732ffa2e4f06d0d6d1f3685a60c126
Felix Favre <gnomus@gnomus.de>
2016-04-14 23:44:07 +0200

    add first 3 statistics

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,2 +1,17 @@
 class StatsController < ApplicationController
+  def index
+
+    @data_instock = Beverage.pluck(:name, :in_storage)
+
+    order_items = OrderItem.where.not(amount: nil).map do |item|
+      [item.beverage.name, item.amount]
+    end
+    @data_ordered = order_items.each_with_object(Hash.new(0)) do |item, memo|
+      memo[item.first] += item.last
+    end
+
+
+    @data_order_time = Order.group("strftime('%Y-%m-%d %H:00', created_at)").count
+
+  end
 end
