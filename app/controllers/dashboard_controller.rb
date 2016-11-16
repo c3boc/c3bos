3006d818b75ed6dc2e5300f72a3eed7592a81076
Felix Favre <gnomus@gnomus.de>
2016-11-16 23:25:29 +0100

    Add "your open orders" table to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,6 @@ class DashboardController < ApplicationController
     @beverages = Beverage.all
     @num_beverages = @beverages.count
     @sum_beverages = @beverages.reduce(0) { |sum, beverage| sum + (beverage['in_storage'] || 0)}
+    @your_orders = Order.where.not(state: :delivered).where(user: current_user).order('created_at desc')
   end
 end
