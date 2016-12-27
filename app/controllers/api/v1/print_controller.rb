4936b588bef23e817f8e6cce534911ae263073c5
gnomus <kontakt@gnomus.de>
2016-12-27 03:36:47 +0100

    Update print_controller.rb

--- app/controllers/api/v1/print_controller.rb
+++ app/controllers/api/v1/print_controller.rb
@@ -2,13 +2,13 @@ class Api::V1::PrintController < Api::V1::BaseController
 
 
   def list
-    @orders = Order.where(printed: false)
+    @orders = Order.where(state: :pending)
     render "print/list"
   end
 
   def set_printed
     @order = Order.find(params[:id])
-    @order.printed = true
+    @order.state = :printed
     @order.save
     render "print/set_printed"
   end
