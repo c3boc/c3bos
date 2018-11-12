e9e50810d7a83badceacd11ec0fde7e6251fe732
Felix Favre <gnomus@gnomus.de>
2018-11-12 17:30:42 +0100

    gem update & smaller fixes

--- Gemfile
+++ Gemfile
@@ -21,6 +21,9 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+gem 'will_paginate', '~> 3.1.0'
+gem 'will_paginate-bootstrap'
+
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,13 +3,13 @@ class OrderItem < ActiveRecord::Base
   belongs_to :beverage
 
   after_save do |order_item|
-    beverage = Beverage.find(order_item.beverage)
+    beverage = Beverage.find(order_item.beverage.id)
     beverage.in_storage = beverage.in_storage - order_item.amount if order_item.amount
     beverage.save
   end
 
   before_destroy do |order_item|
-    beverage = Beverage.find(order_item.beverage)
+    beverage = Beverage.find(order_item.beverage.id)
     beverage.in_storage = beverage.in_storage + order_item.amount if order_item.amount
     beverage.save
   end
