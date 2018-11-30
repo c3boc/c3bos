0b91d4818b13d85478c5d4079785e1613be48a70
Felix Favre <gnomus@gnomus.de>
2018-11-30 18:03:05 +0100

    rails db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,53 +10,65 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_181_112_165_550) do
+ActiveRecord::Schema.define(version: 2018_11_30_165902) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'beverages', id: :serial, force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "beverages", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'locations', id: :serial, force: :cascade do |t|
-    t.string 'title'
-    t.string 'room'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "locations", id: :serial, force: :cascade do |t|
+    t.string "title"
+    t.string "room"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'order_items', id: :serial, force: :cascade do |t|
-    t.integer 'order_id'
-    t.integer 'beverage_id'
-    t.integer 'amount'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['beverage_id'], name: 'index_order_items_on_beverage_id'
-    t.index ['order_id'], name: 'index_order_items_on_order_id'
+  create_table "order_items", id: :serial, force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "beverage_id"
+    t.integer "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["beverage_id"], name: "index_order_items_on_beverage_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
   end
 
-  create_table 'orders', id: :serial, force: :cascade do |t|
-    t.integer 'location_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'user_id'
-    t.integer 'state', default: 0
-    t.index ['location_id'], name: 'index_orders_on_location_id'
-    t.index ['user_id'], name: 'index_orders_on_user_id'
+  create_table "order_permissions", force: :cascade do |t|
+    t.bigint "beverage_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["beverage_id"], name: "index_order_permissions_on_beverage_id"
+    t.index ["user_id"], name: "index_order_permissions_on_user_id"
   end
 
-  create_table 'users', id: :serial, force: :cascade do |t|
-    t.string 'username'
-    t.string 'password_digest'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'role', null: false
+  create_table "orders", id: :serial, force: :cascade do |t|
+    t.integer "location_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "state", default: 0
+    t.index ["location_id"], name: "index_orders_on_location_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
-  add_foreign_key 'order_items', 'beverages'
-  add_foreign_key 'order_items', 'orders'
-  add_foreign_key 'orders', 'locations'
-  add_foreign_key 'orders', 'users'
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "role", null: false
+  end
+
+  add_foreign_key "order_items", "beverages"
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_permissions", "beverages"
+  add_foreign_key "order_permissions", "users"
+  add_foreign_key "orders", "locations"
+  add_foreign_key "orders", "users"
 end
