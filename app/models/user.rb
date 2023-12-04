9629ee0f6c17a985d58976834ba2c69499ff0b5f
Felix Favre <gnomus@gnomus.de>
2023-12-04 15:26:58 +0100

    add role to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
   normalizes :username, with: ->(username) {username.downcase.strip}
+
+  enum role:{
+    guest: 0,
+    bar: 1,
+    supervisor: 2,
+    admin: 3
+  } 
+  
 end
